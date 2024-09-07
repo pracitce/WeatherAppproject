fbf465695323a4466dea2af4fe56e7450371c224
pracitce <109186405+pracitce@users.noreply.github.com>
2024-09-07 11:48:48 -0400

    Fixed some errors JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("hello");
+function handleSearchSubmit {
+event.preventDefault();
+let searchInput = document.querySelector()
+
+}
+
+
+
+
+
+
+
+let searchformElement = document.querySelector("search-form");
+searchformElement.addEventListener("submit", handlesearch);
