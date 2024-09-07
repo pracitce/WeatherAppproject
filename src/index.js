7f7e9e2c66d748157ef54dde34de0110c7f65752
pracitce <109186405+pracitce@users.noreply.github.com>
2024-09-07 16:09:09 -0400

    add more js add temperature and id to the weather number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
+function refreshWeather(response){
+let 
+}
+
+
+
+function searchCity(city){
+let apiKey ="cfab87o4ce22c3f3ec5eaaftfbfbdc0e";
+let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearchSubmit(event){
 event.preventDefault();
 
@@ -5,14 +21,9 @@ let searchInput = document.querySelector("#search-form-input");
 
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
+searchCity(searchInput.value);
 }
 
 
-
-
-
-
-
 let searchFormElement = document.querySelector("#search-form");
-
 searchFormElement.addEventListener("submit",handleSearchSubmit);
