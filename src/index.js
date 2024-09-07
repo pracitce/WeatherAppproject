78b5130f575a0f6dfed3709bdc28c193e6a7591d
pracitce <109186405+pracitce@users.noreply.github.com>
2024-09-07 18:00:35 -0400

    Having problem with apikey not working waiting on customer service

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 
 function refreshWeather(response){
-let 
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+
+
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function searchCity(city) {
+let apiKey = "cfab87o4ce22c3f3ec5eaaftfbfbdc0e";
 
-
-function searchCity(city){
-let apiKey ="cfab87o4ce22c3f3ec5eaaftfbfbdc0e";
-let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
 
 axios.get(apiUrl).then(refreshWeather);
+
 }
 
-
-
-function handleSearchSubmit(event){
+function handleSearchSubmit(event) {
 event.preventDefault();
-
 let searchInput = document.querySelector("#search-form-input");
-
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
 searchCity(searchInput.value);
 }
 
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+
