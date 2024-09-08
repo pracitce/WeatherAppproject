30ef077908f26f5dea2ee576a37bc6d208114103
pracitce <109186405+pracitce@users.noreply.github.com>
2024-09-07 21:22:45 -0400

    update move the cityElement to the top next to the let

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ let temperatureElement = document.querySelector("#temperature");
 let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
 
-
 cityElement.innerHTML = response.data.city;
 temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
-let apiKey = "cfab87o4ce22c3f3ec5eaaftfbfbdc0e";
-
-let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
-
+  let apiKey = "cfab87o4ce22c3f3ec5eaaftfbfbdc0e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(refreshWeather);
 
 }
@@ -22,7 +19,7 @@ function handleSearchSubmit(event) {
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
 let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+
 searchCity(searchInput.value);
 }
 
